482016ba6b462a3f888fcb363417f0cd277c7348
Bruce Chen <662005@qq.com>
2014-06-27 13:33:31 +0800

    update

--- BillionaireGuide.spritebuilder/Scene5.m
+++ BillionaireGuide.spritebuilder/Scene5.m
@@ -13,12 +13,23 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    
+    
+    [self performSelector:@selector(loadScene6) withObject:nil afterDelay:5.f];
 }
 
+- (void)loadScene6 {
+
+    __autoreleasing CCScene *gameplayScene = [CCBReader loadAsScene:@"Scene6"];
+    [[CCDirector  sharedDirector] replaceScene:gameplayScene withTransition:[CCTransition transitionFadeWithDuration:.8f]];
+}
+
+/*
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
 
     __autoreleasing CCScene *gameplayScene = [CCBReader loadAsScene:@"Scene6"];
     [[CCDirector  sharedDirector] replaceScene:gameplayScene withTransition:[CCTransition transitionFadeWithDuration:.8f]];
 }
+ */
 
 @end
