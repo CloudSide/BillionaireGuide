f67d8fc470a7483035cd0bb6a7400c53627653cd
Bruce Chen <662005@qq.com>
2014-10-14 22:44:54 +0800

    CCTableViewExample

--- BillionaireGuide.spritebuilder/Scene4.h
+++ BillionaireGuide.spritebuilder/Scene4.h
@@ -8,6 +8,6 @@
 
 #import "CCNode.h"
 
-@interface Scene4 : CCNode
+@interface Scene4 : CCNode <CCTableViewDataSource>
 
 @end
--- BillionaireGuide.spritebuilder/Scene4.m
+++ BillionaireGuide.spritebuilder/Scene4.m
@@ -4,11 +4,61 @@
 //
 //  Created by Bruce on 14-5-20.
 //  Copyright (c) 2014年 Apportable. All rights reserved.
-//
+//  http://www.lovbl.com/creating-an-in-game-store-with-multiple-tabs-for-cocos2d-using-cctableview/
+//  https://github.com/tudor133/CCTableViewExample
 
 #import "Scene4.h"
 
 @implementation Scene4
+
+- (CCTableViewCell *)tableView:(CCTableView*)tableView nodeForRowAtIndex:(NSUInteger) index {
+    
+    CCTableViewCell* cell = [CCTableViewCell node];
+    cell.contentSizeType = CCSizeTypeMake(CCSizeUnitNormalized, CCSizeUnitUIPoints);
+    cell.contentSize = CGSizeMake(100.0f, 72.0f);
+    float colorFactor = (index / 100);
+    // Just a sample node that changes color with each index value
+    CCNodeColor* colorNode = [CCNodeColor nodeWithColor:[CCColor colorWithRed:colorFactor green:(1.0f - colorFactor) blue:(0.2f + 0.5 * colorFactor) ] width:100.0f height:28.0f];
+    [cell addChild:colorNode];
+    return cell;
+}
+
+- (NSUInteger) tableViewNumberOfRows:(CCTableView *) tableView {
+    
+    return 100;
+    return [[G servers] count]; // just a demo
+}
+
+- (void)didLoadFromCCB {
+    
+    CCTableView *table = [CCTableView node];
+    table.contentSizeType = CCSizeTypeNormalized;
+    table.contentSize = CGSizeMake(1.0, 1.0);
+    table.position = CGPointMake(20, -180);
+    table.dataSource = self; // make our class the data source
+    table.block = ^(CCTableView *table) {
+        NSLog(@"Cell %d was pressed", (int) table.selectedRow);
+    };
+    [self addChild:table];
+}
+
+/*
+- (instancetype)init {
+    
+    self = [super init];
+    
+    if (self) {
+        
+        CCTableView *table = [CCTableView node];
+        table.dataSource = self; // make our class the data source
+        table.block = ^(CCTableView *table) {
+            NSLog(@"Cell %d was pressed", (int) table.selectedRow);
+        };
+        [self addChild:table];
+    }
+    return self;
+}
+ */
 
 - (void)play {
     
